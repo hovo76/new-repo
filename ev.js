45bf98b12bea50bfa99dd98d3404b0412246c939
hovhannes <hovhannes76@gmail.com>
2017-05-18 20:10:27 +0400

    ev.js file added for events

---
+++ ev.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function func(){
+  console.log('Hello');
+}
+
+setInterval(func, 1000);
--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 'use strict';
 
-function show(){
-  console.log("Hello");
+// function show(name){
+//   return function(sname){
+//     console.log("Hello " + name + ' ' + sname);
+//   }
+// }
+//
+// var a = show('valod');
+//
+//   a('vardanyan');
+
+// function q(arr){
+//   if(arr instanceof Array){
+//
+//   }else{
+//     console.log('Sxal e');
+//   }
+// }
+//
+// q(['d', 'hello', 'bye', 'go']);
+
+const speak = function(arr){
+  const newArr = [];
+  for (const i of arr) {
+    newArr.push(i.toUpperCase())
+  }
+  return newArr;
 }
 
-show();
+const res = speak(['hello','world']);
+console.log(res);
